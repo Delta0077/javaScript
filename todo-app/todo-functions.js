1dd101f4c7991f6f92db0909081b49f4851ab945
Delta0077 <adas0262@gmail.com>
2021-06-09 20:23:43 +0530

    creating removeNote ()

--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -14,6 +14,16 @@ const saveTodos = function (todos) {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+// Remove a todo from the list
+const removeTodo = function (id) {
+    const todoIndex = todos.findIndex (function (todo) {
+        return todo.id === id
+    })
+    if (todoIndex > -1) {
+        todos.splice (todoIndex, 1)
+    }
+}
+
 // Render application todos based on filters
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
@@ -55,6 +65,14 @@ const generateTodoDOM = function (todo) {
     // Setup removeButton
     removeButton.textContent = 'x'
     todoText.appendChild(removeButton)
+
+    removeButton.addEventListener('click', function () {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+    
+   
 
     return todoEl
 }
