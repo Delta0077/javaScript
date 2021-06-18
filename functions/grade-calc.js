800fa9421c4bec79e173b64cdd24fb9b9e65901d
Delta0077 <adas0262@gmail.com>
2021-06-19 00:15:33 +0530

    implementing try catch block inside of grade calculator

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -1,22 +1,30 @@
 let grade = function (Score, totalScore)
 {
-    let percent = (Score / totalScore) * 100
-    let letterGrade = ''
-    
-    if (percent >= 90) {
-        letterGrade = 'A'
-    } else if (percent >= 80) {
-        letterGrade = 'B'
-    } else if (percent >= 70) {
-        letterGrade = 'C'
-    } else if (percent >= 60) {
-        letterGrade = 'D'
+    if (typeof Score ==='number' &&  typeof totalScore === 'number') {
+        let percent = (Score / totalScore) * 100
+        let letterGrade = ''
+        
+        if (percent >= 90) {
+            letterGrade = 'A'
+        } else if (percent >= 80) {
+            letterGrade = 'B'
+        } else if (percent >= 70) {
+            letterGrade = 'C'
+        } else if (percent >= 60) {
+            letterGrade = 'D'
+        }
+        else{
+            letterGrade = 'F'
+        }
+        return `You got a ${letterGrade} grade(${percent}%)!`
+    } else {
+        throw Error('Please provide numbers only!!!')
     }
-    else{
-        letterGrade = 'F'
-    }
-    return `You got a ${letterGrade} grade(${percent}%)!`
 }
-
-let result = grade(60, 100)
-console.log(result)
+try {
+    let result = grade(9, 20)
+    console.log(result)
+}
+catch (e) {
+    console.log(e.message)
+}
