ec007c0193bf0af6be6b5b3db26be2e54fe6582c
Delta0077 <adas0262@gmail.com>
2021-07-07 21:01:48 +0530

    HTTP request from javaScript

--- hangman/app.js
+++ hangman/app.js
@@ -8,6 +8,10 @@
 // Number: myNumber --> Number.prototype --> Object.prototype --> null
 // Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
 
+// HTTP
+// Request - what do we want to do
+// Response - What was actually done
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('Cat', 2)
@@ -22,3 +26,16 @@ window.addEventListener ('keypress', function(e) {
     puzzleEl.textContent = game1.getPuzzle()
     guessesEl.textContent = game1.getStatusMessage()
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
