669cf20ef33a97b1c6933872ccaeeb4103198cde
Delta0077 <adas0262@gmail.com>
2021-07-14 02:08:39 +0530

    Http request from js
    1) HTTP request from javaScript
    2) HTTP Headers and errors
    3) Exploring another api

--- hangman/app.js
+++ hangman/app.js
@@ -20,7 +20,7 @@ puzzleEl.textContent = game1.getPuzzle()
 guessesEl.textContent = game1.getStatusMessage()
 
 // Printing on the console which key was pressed
-window.addEventListener ('keypress', function(e) {
+window.addEventListener ('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.getPuzzle()
@@ -31,11 +31,36 @@ window.addEventListener ('keypress', function(e) {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place!')
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
+
+// Challenge 106. Exploring Another API
+
+const countryCode = 'IN'
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    } else if (e.target.readyState === 4) {
+        console.log('Unable to fetch data')
+    }
+})
+
+countryRequest.open('GET' , 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
+if(countryCode) {
+    console.log()
+}
+
+// Status codes https://httpstatuses.com/
