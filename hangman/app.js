e1e2113319cfb7dc94c13b5e3f9766f4a0dd818c
Delta0077 <adas0262@gmail.com>
2021-07-16 23:44:02 +0530

    Callback abstraction

--- hangman/app.js
+++ hangman/app.js
@@ -27,40 +27,37 @@ window.addEventListener ('keypress', (e) => {
     guessesEl.textContent = game1.getStatusMessage()
 })
 
+getPuzzle((error, puzzle) =>{
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
+})
+
+
 // Making an HTTP request
-const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else if (e.target.readyState === 4) {
-        console.log('An error has taken place!')
-    }
-})
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+// // Challenge 106. Exploring Another API
 
-// Challenge 106. Exploring Another API
+// const countryCode = 'IN'
+// const countryRequest = new XMLHttpRequest()
 
-const countryCode = 'IN'
-const countryRequest = new XMLHttpRequest()
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((country) => country.alpha2Code === countryCode)
+//         console.log(country.name)
+//     } else if (e.target.readyState === 4) {
+//         console.log('Unable to fetch data')
+//     }
+// })
 
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((country) => country.alpha2Code === countryCode)
-        console.log(country.name)
-    } else if (e.target.readyState === 4) {
-        console.log('Unable to fetch data')
-    }
-})
-
-countryRequest.open('GET' , 'http://restcountries.eu/rest/v2/all')
-countryRequest.send()
-if(countryCode) {
-    console.log()
-}
+// countryRequest.open('GET' , 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
+// if(countryCode) {
+//     console.log()
+// }
 
 // Status codes https://httpstatuses.com/
