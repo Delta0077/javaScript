01dbc7a7a561c4119f2e7a9f2e1e652f95f78ad5
Delta0077 <adas0262@gmail.com>
2021-07-22 00:05:45 +0530

    Asynchronous vs. Synchronous Execution
    1)Asynchronous vs. Synchronous Execution
    2)Callback Abstraction Challenge

--- hangman/app.js
+++ hangman/app.js
@@ -27,7 +27,7 @@ window.addEventListener ('keypress', (e) => {
     guessesEl.textContent = game1.getStatusMessage()
 })
 
-getPuzzle((error, puzzle) =>{
+getPuzzle("2",(error, puzzle) =>{
     if (error) {
         console.log(`Error: ${error}`)
     } else {
@@ -35,27 +35,18 @@ getPuzzle((error, puzzle) =>{
     }
 })
 
+getCountry('IN', (error, country) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(`Country name: ${country.name}`)
+    }
+})
 
 // Making an HTTP request
 
 
-// // Challenge 106. Exploring Another API
 
-// const countryCode = 'IN'
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const country = data.find((country) => country.alpha2Code === countryCode)
-//         console.log(country.name)
-//     } else if (e.target.readyState === 4) {
-//         console.log('Unable to fetch data')
-//     }
-// })
-
-// countryRequest.open('GET' , 'http://restcountries.eu/rest/v2/all')
-// countryRequest.send()
 // if(countryCode) {
 //     console.log()
 // }
