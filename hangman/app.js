b49d3bd7b27b39825737c3a7836ebf44e205697c
Delta0077 <adas0262@gmail.com>
2021-08-06 01:34:27 +0530

    promise chaining

--- hangman/app.js
+++ hangman/app.js
@@ -29,22 +29,24 @@ window.addEventListener ('keypress', (e) => {
 
 getPuzzle('2').then((puzzle) => {
     console.log(puzzle)
-}, (err) => {
+}).catch((err) => {
     console.log(`Error: ${err}`)
 })
 
-getCountry('IN').then((country) => {
-    console.log(country.name)
-}, (err) => {
-    console.log(`Error: ${err}`)
-})
+// getCountry('IN').then((country) => {
+//     console.log(country.name)
+// }, (err) => {
+//     console.log(`Error: ${err}`)
+// })
 
-// Making an HTTP request
-
-
-
-// if(countryCode) {
-//     console.log()
-// }
-
-// Status codes https://httpstatuses.com/
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//     if (response.status === 200) {
+//         return response.json()
+//     } else {
+//         throw new Error('Unable to fetch the puzzle')
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error)
+// })
