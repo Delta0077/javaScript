125c0e2c18b1cafcbe374a9d3936903227beb93c
Aditya <adas0262@gmail.com>
2021-08-13 22:15:03 +0530

    async/await

---
+++ functions/async-await.js
@@ -0,0 +1,19 @@
+const getDataPromise = (num) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+       typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
+    }, 2000)
+})
+
+const processData = async () => {
+    await getDataPromise(2) //
+    
+    // getDataPromise(2).then((data) => {
+    //     console.log(data)
+    // })
+}
+
+processData().then((data) => {
+    console.log('Data', data)
+}).catch((error) => {
+    console.log('Error', error)
+})
