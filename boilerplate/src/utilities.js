b4fb86cb8050077d8e2c742e19be8ee30f68ca19
Aditya <adas0262@gmail.com>
2021-08-31 01:11:18 +0530

    Update utilities.js

--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,5 +1,10 @@
 // Named Export
 // Default export
 
-export const add = (a, b) => a + b // exporting add function so we can use it into another file
-export const name = 'Aditya'
+const add = (a, b) => a + b // exporting add function so we can use it into another file
+const name = 'Aditya'
+
+const square = (x) => x * x
+// export default square // default export syntax
+
+export { add, name, square as default } // Syntax to export in a single line
