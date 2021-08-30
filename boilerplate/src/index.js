02caa1ce33258c62a476e370f8050c4a5d2063c0
Aditya <adas0262@gmail.com>
2021-08-31 00:47:03 +0530

    description

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,5 +1,5 @@
-import { add, name } from './utilities' // syntax for accessing any function from another js file, we can access multiple function just by seperating with a comma
-import { scream } from './scream'
+import { add, name } from './utilities' // This is called Named export. syntax for accessing any function from another js file, we can access multiple function just by seperating with a comma.
+import { scream } from './scream' // named export
 console.log('index.js')
 console.log(add(32, 1))
 console.log(scream(name))
--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,4 +1,5 @@
-console.log('utilities.js')
+// Named Export
+// Default export
 
 export const add = (a, b) => a + b // exporting add function so we can use it into another file
 export const name = 'Aditya'
