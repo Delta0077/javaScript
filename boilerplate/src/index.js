e240fa75b58ac9fd07ca5d0b14ad16a3369e4d27
Aditya <adas0262@gmail.com>
2021-08-31 01:45:26 +0530

    webpack dev-server setup
    npm run dev-server

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,6 +1,6 @@
 import otherSquare, /*default export*/  { add, name } from './utilities' // This is called Named export. syntax for accessing any function from another js file, we can access multiple function just by seperating with a comma.
 import secondScream from './scream' // named
 console.log('index.js')
-console.log(add(32, 1))
+console.log(add(32, 6))
 console.log(secondScream(name))
 console.log(otherSquare(10))
--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -2,7 +2,7 @@
 // Default export
 
 const add = (a, b) => a + b // exporting add function so we can use it into another file
-const name = 'Aditya'
+const name = 'Aditya Das'
 
 const square = (x) => x * x
 // export default square // default export syntax
